8ffa5cfbb1ad5da25910463ceb0dcb1f5a98d939
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-04 21:42:48 +0100

    Update cctv-hack.py

--- cctv-hack.py
+++ cctv-hack.py
@@ -50,19 +50,24 @@ if cho=="1":
     print(Yellow,bold,"1:-United States\n2:-Mexico\n3:-Moldova\n4:-Japan\n5:-Finland\n5:-Nicaragua\n6:-Italy\n7:-China\n8:-Malta\n9:-Korea\n10:-Chile")
     try:
     
-      coun= ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL","-"]
+      countries= ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL","-"]
       headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
       num= int(input("Enter Country: "))
       if num not in range(1, 10+1):
         raise IndexError
-      country = coun[num-1]
-      res = requests.get(f"https://www.insecam.org/en/bycountry/{country}", headers=headers)
-      last = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+      country = countries[num-1]
+      res = requests.get(
+        f"https://www.insecam.org/en/bycountry/{country}", headers=headers
+    )
+      last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
 
-      for page in range(int(last)):
-         res= requests.get(f"https://www.insecam.org/en/bycountry/{country}/?page={page}",headers=headers)
-      	 find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
-         for ip in find_ip:
+      for page in range(int(last_page)):
+        res = requests.get(
+            f"https://www.insecam.org/en/bycountry/{country}/?page={page}",
+            headers=headers
+        )
+        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+        for ip in find_ip:
             print("\033[1;31m", ip)
     except:
       pass
