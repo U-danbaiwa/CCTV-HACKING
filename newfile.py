2bfa794e6eddf89f1f3ada2bdfd310c999026c36
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-04 20:32:29 +0100

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -2,9 +2,10 @@ import requests,re,colorama
 colorama.init()
 a=input("Enter option: ")
 if a=="1":
-    try:
-    	print()
-    	countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
+   
+   try:
+    print()
+    countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
                  "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "PL", "IR",
                  "NO", "RO", "IN", "VN", "BE", "BR", "BG", "ID", "DK", "AR",
                  "MX", "FI", "CN", "CL", "ZA", "SK", "HU", "IE", "EG", "TH",
@@ -14,35 +15,35 @@ if a=="1":
                  "KW", "VE", "GE", "ME", "SV", "LU", "CW", "PR", "CR", "BY",
                  "AL", "LI", "BA", "PY", "PH", "FO", "GT", "NP", "PE", "UY",
                  "-"]
-    	headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
 
-   	 num = int(input("OPTIONS : "))
-   	 if num not in range(1, 91+1):
-        	raise IndexError
+    num = int(input("OPTIONS : "))
+    if num not in range(1, 91+1):
+      raise IndexError
 
-    	country = countries[num-1]
-	    res = requests.get(
+    country = countries[num-1]
+    res = requests.get(
         f"https://www.insecam.org/en/bycountry/{country}", headers=headers
     )
-    	last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+    last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
 
-  	  for page in range(int(last_page)):
-    	    res = requests.get(
+    for page in range(int(last_page)):
+      res = requests.get(
             f"https://www.insecam.org/en/bycountry/{country}/?page={page}",
             headers=headers
         )
-       	 find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
-        	for ip in find_ip:
-            	print("\033[1;31m", ip)
-	except:
-   	 pass
-	finally:
-    	print("\033[1;37m")
-  	  exit()
+      find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+        for ip in find_ip:
+        print("\033[1;31m", ip)
+   except:
+    pass
+   finally:
+    print("\033[1;37m")
+    exit()
 elif a=="2":
-	try:
-    	print()
-    	countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
+   try:
+    print()
+    countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
                  "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "PL", "IR",
                  "NO", "RO", "IN", "VN", "BE", "BR", "BG", "ID", "DK", "AR",
                  "MX", "FI", "CN", "CL", "ZA", "SK", "HU", "IE", "EG", "TH",
@@ -50,30 +51,30 @@ elif a=="2":
                  "TN", "EE", "DO", "SI", "EC", "LT", "PS", "NZ", "BD", "PA",
                  "MD", "NI", "MT", "IT", "SA", "HR", "CY", "PK", "AE", "KZ",
                  "KW", "VE", "GE", "ME", "SV", "LU", "CW", "PR", "CR","-"]
-    	headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
 
-   	 num = int(input("OPTIONS : "))
-   	 if num not in range(1, 70+1):
-        	raise IndexError
+    num = int(input("OPTIONS : "))
+    if num not in range(1, 70+1):
+        raise IndexError
 
-    	country = countries[num-1]
+    country = countries[num-1]
 	    res = requests.get(
         f"https://www.insecam.org/en/bycountry/{country}", headers=headers
     )
-    	last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+    last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
 
-  	  for page in range(int(last_page)):
-    	    res = requests.get(
+    for page in range(int(last_page)):
+      res = requests.get(
             f"https://www.insecam.org/en/bycountry/{country}/?page={page}",
             headers=headers
         )
-       	 find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
-        	for ip in find_ip:
-            	print("\033[1;31m", ip)
-	except:
-   	 pass
-	finally:
-    	print("\033[1;37m")
-  	  exit()
+      find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+      for ip in find_ip:
+        print("\033[1;31m", ip)
+   except:
+    pass
+   finally:
+    print("\033[1;37m")
+    exit()
 else:
-	print("sorry")
+   print("sorry")
