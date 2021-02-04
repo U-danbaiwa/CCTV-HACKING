5de7bc9b950ec14b4d5cc034c24e701e628682c5
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-04 21:54:27 +0100

    Update cctv-hack.py

--- cctv-hack.py
+++ cctv-hack.py
@@ -231,7 +231,7 @@ if cho=="1":
        res= requests.get(f"https://www.insecam.org/en/bycountry/{country}/?page={page}",headers=headers)
        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
        for ip in find_ip:
-       print('\033[096m',"CCTV IP HACKED==>","\033[92m",'\033[01m',ip,green,"ACCESS GRANTED")		      
+         print('\033[096m',"CCTV IP HACKED==>","\033[92m",'\033[01m',ip,green,"ACCESS GRANTED")		      
 			
     except:
       pass
