eb38c2a7b832280818cfeba777ecd3d5ba0f1f19
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-04 11:47:09 +0100

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -14,7 +14,7 @@ os.system("clear")
 os.system("figlet U-danbaiwa")
 print(green,"\t\t\tv1.0.0")
 print("\n\n")
-print(cyan,"1:-Hack Cctv Camera")
+print(cyan," 1:-Hack Cctv Camera")
 print("")
 print(yellow,bold,"2:-Update Tool")
 print("")
@@ -22,41 +22,44 @@ print(cy,bold,"3:-Exit")
 print("\n")
 cho=input(cya,bold,"Enter Option: ")      
 if cho=="1":
-  
-  try:
-    print()
-    countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
-                 "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "PL", "IR",
-                 "NO", "RO", "IN", "VN", "BE", "BR", "BG", "ID", "DK", "AR",
-                 "MX", "FI", "CN", "CL", "ZA", "SK", "HU", "IE", "EG", "TH",
-                 "UA", "RS", "HK", "GR", "PT", "LV", "SG", "IS", "MY", "CO",
-                 "TN", "EE", "DO", "SI", "EC", "LT", "PS", "NZ", "BD", "PA",
-                 "MD", "NI", "MT", "IT", "SA", "HR", "CY", "PK", "AE", "KZ",
-                 "KW", "VE", "GE", "ME", "SV", "LU", "CW", "PR", "CR", "BY",
-                 "AL", "LI", "BA", "PY", "PH", "FO", "GT", "NP", "PE", "UY",
-                 "-"]
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
+   
+   print("\n")
+   print(red,"1:-France")
+   print(green,"2:-South Africa")
+   print(yellow,"3:-Saudi Arabia")
+   print(cya,"4:-Germany")
+   print(bold,"5:-Slovakia")
+   print(cya,"6:-Crotia")
+   print(green,"7:-Taiwan")
+   print(red,"8:-Hungary")
+   print(cya,"9:-Cyprus")
+   print(yellow,"10:-Russian Federation")
+   try:
+     print()
+     countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL","-"]
+     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
 
-    num = int(input("OPTIONS : "))
-    if num not in range(1, 91+1):
-        raise IndexError
+     num = int(input("OPTIONS : "))
+     if num not in range(1, 10+1):
+       raise IndexError
 
-    country = countries[num-1]
-    res = requests.get(
+     country = countries[num-1]
+     res = requests.get(
         f"https://www.insecam.org/en/bycountry/{country}", headers=headers
     )
-    last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+     last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
 
-    for page in range(int(last_page)):
-        res = requests.get(
+     for page in range(int(last_page)):
+       res = requests.get(
             f"https://www.insecam.org/en/bycountry/{country}/?page={page}",
             headers=headers
         )
-        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
-        for ip in find_ip:
-            print("\033[1;31m", ip)
-  except:
-    pass
-  finally:
-    print("\033[1;37m")
-    exit()
+       find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+       for ip in find_ip:
+         print("\033[1;31m", ip)
+   
+   except:
+     pass
+   finally:
+     print("\033[1;37m")
+     exit()
